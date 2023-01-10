9422f0aa4858fd3000f541a06aab701fb5582ad5
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2023-01-11 00:53:05 +1000

    Added solution

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from telegram import Bot
-from telegram.ext import Updater
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
+from random import randint
+import functions
 
-# TOKEN = 5883434456:AAE_hP3ksro3k54qU1I794bo1brDN2u9MuQ
+app = ApplicationBuilder().token('5883434456:AAE_hP3ksro3k54qU1I794bo1brDN2u9MuQ').build()
 
-bot = Bot(token='5883434456:AAE_hP3ksro3k54qU1I794bo1brDN2u9MuQ')
-updater = Updater(token='5883434456:AAE_hP3ksro3k54qU1I794bo1brDN2u9MuQ')
-dispatcher = updater.dispatcher
-updater.start_polling()
+app.add_handler(ConversationHandler(entry_points= [CommandHandler('game', functions.game)], 
+                                    states={functions.my_turn:[MessageHandler(filters.TEXT, functions.gamer_turn)], 
+                                           functions.bot:[MessageHandler(filters.TEXT, functions.bot_turn)]},
+                                    fallbacks=[CommandHandler('cancel', functions.cancel)]))
 
+app.add_handler(CommandHandler('hello', functions.hello))
+app.add_handler(MessageHandler(filters.TEXT, functions.del_word))
+
+
+app.run_polling()
